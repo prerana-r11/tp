871d3a39abbf540af5f4bdd934f97740b15765c6
prerana-r11 <preranaravi2006@gmail.com>
2026-03-17 15:58:12 +0800

    Implement add Gift command

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -2,14 +2,11 @@
 // se-edu/addressbook-level2/blob/master/src/seedu/addressbook/parser/Parser.java and supervision from the author
 package seedu.duke.parser;
 
-import seedu.duke.commands.ActionCommand;
-import seedu.duke.commands.ChildCommand;
-import seedu.duke.commands.ChildListCommand;
-import seedu.duke.commands.ElfListCommand;
-import seedu.duke.commands.FindCommand;
-import seedu.duke.commands.Command;
+import seedu.duke.commands.*;
 
 import seedu.duke.data.exception.IllegalValueException;
+
+import java.util.ArrayList;
 
 public class Parser {
     public Command parseCommand(String userInput) throws IllegalValueException {
@@ -18,28 +15,31 @@ public class Parser {
         String commandWord = parts[0];
         String arguments = parts.length > 1 ? parts[1] : "";
 
-        
-        switch (commandWord) {
-        
-        case "child":
-            return prepareAdd(arguments);
-        
-        case "childlist":
-            return new ChildListCommand();
-        
-        case "elflist":
-            return new ElfListCommand();
-            
-        case "find":
-            return new FindCommand(arguments);
 
-        //@@author GShubhan
-        case "action":
-            return prepareAction(arguments);
-        //@@author
-            
-        default:
-            throw new IllegalValueException("Unknown command. Did you mean 'child' or 'childlist'?");
+        switch (commandWord) {
+            case "child":
+                return prepareAdd(arguments);
+
+            case "childlist":
+                return new ChildListCommand();
+
+            case "elflist":
+                return new ElfListCommand();
+
+            case "find":
+                return new FindCommand(arguments);
+
+            //@@author GShubhan
+            case "action":
+                return prepareAction(arguments);
+            //@@author
+            case "gift":
+                return prepareGiftAction(arguments);
+
+
+
+            default:
+                throw new IllegalValueException("Unknown command. Did you mean 'child' or 'childlist'?");
         }
     }
 
@@ -84,5 +84,28 @@ public class Parser {
         } catch (Exception e) {
             throw new IllegalValueException("Format: action CHILD_INDEX a/ACTION s/SEVERITY");
         }
+    }
+
+    private GiftCommand prepareGiftAction(String args) throws IllegalValueException {
+        try{
+            String[] parts=args.trim().split(" ");
+            int childIndex= Integer.parseInt(parts[0])-1;
+            ArrayList<String> giftNames= new ArrayList<>();
+
+            for(int i=1;i<parts.length;i++){
+                if(parts[i].startsWith("g/")){
+                    giftNames.add(parts[i].substring(2));
+                }
+            }
+
+            if(giftNames.isEmpty()){
+                throw new IllegalValueException("Please enter gift names:)");
+            }
+            return new GiftCommand(childIndex, giftNames);
+        } catch (NumberFormatException e) {
+            throw new IllegalValueException("Please use valid command format : g/");
+        }
+
+
     }
 }
