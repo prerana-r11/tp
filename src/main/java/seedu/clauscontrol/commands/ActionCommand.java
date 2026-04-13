8276a86d4114eb0eba9057d548695702ae619756
GShubhan <ShubhanGabra01@gmail.com>
2026-04-14 02:40:54 +0800

    Add zero severity action warning

--- src/main/java/seedu/clauscontrol/commands/ActionCommand.java
+++ src/main/java/seedu/clauscontrol/commands/ActionCommand.java
@@ -9,6 +9,9 @@ import seedu.clauscontrol.data.child.Child;
 
 public class ActionCommand extends Command {
     public static final String SUCCESS = "Action added for %s: \"%s\" (severity: %d)";
+    public static final String SUCCESS_ZERO_SEVERITY =
+            "Action added for %s: \"%s\" (severity: 0)\n"
+                    + "Note: A severity of 0 has no effect on this child's nice/naughty score.";
     public static final String INVALID_INDEX = "Enter a valid child index";
 
     private int childIndex;
@@ -37,6 +40,9 @@ public class ActionCommand extends Command {
             Child child = childList.get(childIndex - 1);
             assert child != null : "Child should not be null";
             child.addAction(action, severity);
+            if (severity == 0) {
+                return String.format(SUCCESS_ZERO_SEVERITY, child.getName(), action);
+            }
             return String.format(SUCCESS, child.getName(), action, severity);
         } catch (Exception e) {
             return "Something went wrong adding the action!";
